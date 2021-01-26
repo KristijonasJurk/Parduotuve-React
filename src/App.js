2a9b2c4327385e6acb875e9b0585667807ec3ff2
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-01-26 21:46:52 +0200

    adding main content

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
+import { render } from '@testing-library/react';
 import React from 'react';
+import data from "./data.json";
 
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/">Parduotuvė</a>
-      </header>
-      <main>
-        product list
-      </main>
-      <footer>
-        all rights reserved
+class App extend React.Component {
+  constructor(){
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: ""
+    }
+  }
+
+  render(){
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">Parduotuvė</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              products
+            </div>
+            <div className="sidebar">
+              cart items
+            </div>
+          </div>
+        </main>
+        <footer>
+          all rights reserved
       </footer>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
