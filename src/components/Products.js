9300d7350407e5b62499c962a41c922136ef500d
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-01-26 23:44:15 +0200

    css of product

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,4 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+function formatCurrency(num) {
+    return Number(num.toFixed(0)).toLocaleString() + ".";
+}
+function formatCurrencySmall(num) {
+    return Number(num.toFixed(2)).toLocaleString().slice(3, 5);
+}
+
 
 export default class Products extends Component {
     render() {
@@ -15,8 +23,16 @@ export default class Products extends Component {
                                     </p>
                                 </a>
                                 <div className="product-price">
-                                    <div>
-                                        {product.price}
+                                    <div className="price-tag">
+                                        <div className="smaller">
+                                            $
+                                        </div>
+                                        <div>
+                                            {formatCurrency(product.price)}
+                                        </div>
+                                        <div className="smaller">
+                                            {formatCurrencySmall(product.price)}
+                                        </div>
                                     </div>
                                     <button className="button primary">
                                         Add to Cart
